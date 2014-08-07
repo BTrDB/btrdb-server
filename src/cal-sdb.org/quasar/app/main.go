0058fcad42ee4b87d576046df8694487385e1870
Michael Andersen <m.andersen@cs.berkeley.edu>
2014-08-07 15:34:46 -0700

    Add profiler support

--- src/cal-sdb.org/quasar/app/main.go
+++ src/cal-sdb.org/quasar/app/main.go
@@ -8,19 +8,32 @@ import (
 	"cal-sdb.org/quasar/httpinterface"
 	"cal-sdb.org/quasar/cpinterface"
 	"time"
+	"runtime/pprof"
+	"os"
 	//"code.google.com/p/go-uuid/uuid"
 )
 
 var serveHttp = flag.String("http", "", "Serve requests from this address:port")
 var serveCPNP = flag.String("cpnp", "localhost:4410", "Serve Capn Proto requests over this port")
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
+    
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+        f, err := os.Create(*cpuprofile)
+        if err != nil {
+            log.Fatal(err)
+        }
+        pprof.StartCPUProfile(f)
+        defer pprof.StopCPUProfile()
+    }
+
 	q, err := quasar.NewQuasar(&quasar.DefaultQuasarConfig)
 	if err != nil {
 		log.Panic(err)
 	}
 	
-	flag.Parse()
 	if *serveHttp != "" {
 		go httpinterface.QuasarServeHTTP(q, *serveHttp)
 	}
