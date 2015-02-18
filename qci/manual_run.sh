d669f13321abdef45c8653159640404427581988
Sam Kumar <samkumar@berkeley.edu>
2015-02-18 14:28:22 -0800

    Made it possible to test with filestore instead of ceph; added test for extra/missing points at end of statistical query

--- qci/manual_run.sh
+++ qci/manual_run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -ex
 rm -f profile.*
 rm -f log.*
-export goversion=go_64_1.4.1
+export goversion=go_64_1.4.2
 export GOROOT=/srv/$goversion
 export GO=$GOROOT/bin/go
-mkdir gopath
+mkdir -p gopath
 export GOPATH=`pwd`/gopath
 export PATH=$PATH:$GOROOT/bin/
 git pull
 $GO get -v -d ./...
 $GO build -a -v -o exe ./qserver
+export STORE=filestore
 export CEPHTYPE=primary
-export TEST_TYPE=readwrite2
+export TEST_TYPE=endpoint
 ipython qci/runtests.ipy
