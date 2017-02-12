d989de71b43e3e1f66374c90a7dadb5f9054a878
Michael Andersen <m.andersen@cs.berkeley.edu>
2017-02-12 13:07:50 -0800

    foo

--- k8scontainer/rebuild.sh
+++ k8scontainer/rebuild.sh
@@ -8,7 +8,7 @@ ver=$(../btrdbd/btrdbd -version)
 cp ../btrdbd/btrdbd .
 cp `which panicparse` .
 
-docker build  -t btrdb/k8s:${ver} .
+docker build --no-cache  -t btrdb/k8s:${ver} .
 docker push btrdb/k8s:${ver}
 docker tag btrdb/k8s:${ver} btrdb/k8s:latest
 docker push btrdb/k8s:latest
