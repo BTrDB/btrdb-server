7155db2b12be40d6c58b586884d6259f5d16431b
Michael Andersen <m.andersen@cs.berkeley.edu>
2017-04-03 12:19:11 -0700

    bump

--- version/consts.go
+++ version/consts.go
@@ -4,7 +4,7 @@ import "fmt"
 
 const Major = 4
 const Minor = 4
-const Subminor = 7
+const Subminor = 8
 
 //Will be set at build time to Major.Minor.Build
 var VersionString = fmt.Sprintf("%d.%d.%d", Major, Minor, Subminor)
