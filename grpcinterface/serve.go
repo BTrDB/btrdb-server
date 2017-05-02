c5b91ce6a820884093a85a68c37616e850a7d4b2
Michael Andersen <m.andersen@cs.berkeley.edu>
2017-05-02 13:28:56 -0700

    Remove gateway generation

--- grpcinterface/serve.go
+++ grpcinterface/serve.go
@@ -30,7 +30,7 @@ func init() {
 }
 
 //go:generate protoc -I/usr/local/include -I. -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=Mgoogle/api/annotations.proto=github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:. btrdb.proto
-//go:generate protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis  --grpc-gateway_out=logtostderr=true:.  btrdb.proto
+// // # go:generate protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis  --grpc-gateway_out=logtostderr=true:.  btrdb.proto
 
 const MaxOpTime = 60 * time.Second
 
