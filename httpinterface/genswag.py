0db5de57cf1024680b83d64709685e92d0baf9ce
Michael Andersen <m.andersen@cs.berkeley.edu>
2017-05-24 15:23:40 -0700

    Remove httpinterface

--- httpinterface/genswag.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-f = open("swagger.json.go","w")
-f.write("package httpinterface\n")
-swag = open("btrdb.swagger.json").read()
-f.write("const SwaggerJSON = `")
-f.write(swag)
-f.write("`;")
-f.close()
