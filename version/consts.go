216c59519e10fcd9b0d0f41ce100e740cb5d6cb8
Michael Andersen <michael@steelcode.com>
2018-07-05 16:56:00 -0700

    Version bump

--- version/consts.go
+++ version/consts.go
@@ -3,8 +3,8 @@ package version
 import "fmt"
 
 const Major = 4
-const Minor = 11
-const Subminor = 1
+const Minor = 12
+const Subminor = 0
 
 var VersionString = fmt.Sprintf("%d.%d.%d", Major, Minor, Subminor)
 var BuildDate string
