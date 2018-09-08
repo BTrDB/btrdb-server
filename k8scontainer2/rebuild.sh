5dbd2d6948d847e5cd0a785c08ee92cab0bfead7
Michael P Andersen <michael@steelcode.com>
2018-09-08 15:55:01 -0700

    Add script

---
+++ k8scontainer2/rebuild.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+pushd ../btrdbd/
+go build -v 
+popd
+ver=$(../btrdbd/btrdbd -version)
+cp ../btrdbd/btrdbd .
+cp `which panicparse` .
+
+docker build -t btrdb/dev-db:${ver} .
+docker push btrdb/dev-db:${ver}
+docker tag btrdb/dev-db:${ver} btrdb/dev-db:latest
+docker push btrdb/dev-db:latest
